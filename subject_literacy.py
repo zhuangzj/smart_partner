8e31973132cf5e65cc821e8381fda18027cc2218
zzj <buu2012victoria@163.com>
2017-07-20 17:53:49 +0800

    tidy

--- subject_literacy.py
+++ subject_literacy.py
@@ -9,35 +9,14 @@ import pandas as pd
 import numpy as np
 
 def main():
-    df01 = pd.read_excel('./data/subject_quality/grade8-math201701.xls', skiprows=1)
-    df05 = pd.read_excel('./data/subject_quality/grade8-math201705.xls', skiprows=1)
-    df07 = pd.read_excel('./data/subject_quality/grade8-math201707.xls')
-    
-#    df01 = df01.iloc[1:, 41:69]
-#    df01.columns = ['直观', '直观', '运算', '直观', '推理', '直观', '运算', '数据处理', '推理', '推理', '运算', '运算', '推理', '推理', '推理', '直观', '运算', '运算', '推理', '运算', '推理', '推理', '推理', '推理', '建模', '推理', '推理', '运算']
-    # drop text
-#    df01.drop(df01.index[-2], inplace=True)
-#    df01 = df01[:-1].div(df01.iloc[-1, :])
-    # .mean() only numeric type avaliable
-#    df01 = df01.groupby(df01.columns, axis=1).apply(np.mean, axis=1)
-#    df01 = data_process(df01, -2)
-#    print(df01)
-    
-#    stu_df = df05.iloc[1:, 0:2]
-#    stu_df.columns = ['教育ID', '姓名']
-#    stu_df['教育ID'] = stu_df['教育ID'][:-2].astype(int).astype(str)
-#    print(stu_df)
-#    df05 = df05.iloc[1:, 39:65]
-#    df05.columns = ['推理', '推理', '推理', '直观', '直观', '推理', '直观', '推理', '推理', '建模', '推理', '推理', '运算', '直观', '建模', '建模', '直观', '直观', '运算', '推理', '运算', '建模', '运算', '直观', '推理', '推理']
-#    df05 = pd.merge(stu_df, df05, left_index=True, right_index=True)
-#    df05.drop(df05.index[-1], inplace=True)
+    df01 = pd.read_excel('./data/subject_literacy/grade8-math201701.xls', skiprows=1)
+    df05 = pd.read_excel('./data/subject_literacy/grade8-math201705.xls', skiprows=1)
+    df07 = pd.read_excel('./data/subject_literacy/grade8-math201707.xls', skiprows=1)
     
     df01 = data_process(df01, 41, 69, ['直观', '直观', '运算', '直观', '推理', '直观', '运算', '数据处理', '推理', '推理', '运算', '运算', '推理', '推理', '推理', '直观', '运算', '运算', '推理', '运算', '推理', '推理', '推理', '推理', '建模', '推理', '推理', '运算'], -2)
     df05 = data_process(df05, 39, 65, ['推理', '推理', '推理', '直观', '直观', '推理', '直观', '推理', '推理', '建模', '推理', '推理', '运算', '直观', '建模', '建模', '直观', '直观', '运算', '推理', '运算', '建模', '运算', '直观', '推理', '推理'], -1)
-    print(df01)
-#    df05 = data_process(df05, -1)
-    
-    #print(df05)
+    df07 = data_process(df07, 48, 83, ['推理', '直观', '运算', '建模', '数据处理', '推理', '推理', '建模', '推理', '直观', '直观', '运算', '运算', '运算', '推理', '推理', '运算', '运算', '运算', '运算', '推理', '运算', '运算', '建模', '数据处理', '数据处理', '数据处理', '推理', '推理', '推理', '推理', '推理', '运算', '运算', '运算'], -1)
+    #print(df07)
  
    
 def data_process(df, start, end, columns, drop_row):
